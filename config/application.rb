36e00e599ae47e7907b5ad6847595257dbfb77d7
Martin Svalin <martin@lite.nu>
2010-11-02 16:02:33 +0100

    Changed generator defaults to rspec and factory girl. Cleanup in Gemfile

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Mytraining
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Stockholm'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -38,5 +38,11 @@ module Mytraining
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    # Configure generators values. Many other options are available, be sure to check the documentation.
+    config.generators do |g|
+      g.test_framework  :rspec
+      g.fixture_replacement :factory_girl
+    end
   end
 end
