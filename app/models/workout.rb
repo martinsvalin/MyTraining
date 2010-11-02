9f4f07694fe7b3f646d0838570bbe824334380a5
Martin Svalin <martin@lite.nu>
2010-11-02 16:45:32 +0100

    added associations between person <-> workout and workout <-> training. Fixed migration error.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,4 @@
 class Workout < ActiveRecord::Base
+  belongs_to :person
+  belongs_to :training
 end
--- db/migrate/20101102152250_create_workouts.rb
+++ db/migrate/20101102152250_create_workouts.rb
@@ -1,9 +1,9 @@
 class CreateWorkouts < ActiveRecord::Migration
   def self.up
     create_table :workouts do |t|
-      t.reference :person
-      t.reference :training
-      t.int :points
+      t.integer :person_id
+      t.integer :training_id
+      t.integer :points
 
       t.timestamps
     end
