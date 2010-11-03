515a10a01a2d80e2944ddafafa463f629a809ffd
Joakim Westerlund <joakim.westerlund@gmail.com>
2010-11-03 09:48:22 +0100

    Set workout points on create.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,18 @@
 class Workout < ActiveRecord::Base
   belongs_to :person
   belongs_to :training
+
+  before_validation :set_points, :on => :create
+
+  validates_numericality_of :points
+  
+  private
+    def set_points
+      self.points = training.workouts.count >= 4 ? set_group_points_for_everyone : 1000
+    end
+    
+    def set_group_points_for_everyone
+      training.workouts.map {|workout| workout.update_attribute :points, 5000}
+      5000
+    end
 end
