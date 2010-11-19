0c13dabd6c2dc07c1b8b4a294476241121ef1033
Joakim Westerlund <joakim.westerlund@gmail.com>
2010-11-19 11:00:36 +0100

    Toggle workout link works

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+    $('a.enable-disable').click(function() {
+        if($(this).data('status') == 'enabled') {
+            $('this').data('status','disabled');
+            return true;
+        } else {
+            $(this).parents('form').submit();
+            $(this).data('status','enabled');
+            return false;
+        }
+    });
+});
