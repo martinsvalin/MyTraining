cf7e8613fd66ba4a44679f96695e9ad4ca262b41
Joakim Westerlund <joakim.westerlund@gmail.com>
2010-11-22 09:11:04 +0100

    removed unused checkbox for activity state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
   def toggle_button(enabled = false)
     return %Q{
       <label class="cb-enable#{" selected" if enabled}"><span>On</span></label>
-      <label class="cb-disable#{" selected" unless enabled}"><span>Off</span></label>
-      <input type="checkbox" id="checkbox" class="checkbox" />}.html_safe
+      <label class="cb-disable#{" selected" unless enabled}"><span>Off</span></label>}.html_safe
   end
 end
