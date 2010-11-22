d104f036ebde9daf20e19ff9083d037ec6e7fbf6
Joakim Westerlund <joakim.westerlund@gmail.com>
2010-11-22 20:47:57 +0100

    updated css and mark-up. Also changed the on/off buttons just for testing.
    Also implemented buttons and changed the width of input elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def toggle_button(enabled = false)
     return %Q{
-      <label class="cb-enable#{" selected" if enabled}"><span>On</span></label>
-      <label class="cb-disable#{" selected" unless enabled}"><span>Off</span></label>}.html_safe
+      <label class="cb-enable#{" selected" if enabled}"><span>#{enabled ? "|" : "&#160;"}</span></label>
+      <label class="cb-disable#{" selected" unless enabled}"><span>#{!enabled ? "O" : "&#160;"}</span></label>}.html_safe
   end
 end
