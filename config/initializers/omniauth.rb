813d278a5a6d4926c844adb4933b45c825db7f86
Martin Svalin <martin@lite.nu>
2010-12-08 14:54:46 +0100

    Fixed default google_domain for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   require 'memcached'
   require "openid/store/memcache"
   # provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
-  provider :GoogleApps, OpenID::Store::Memcache.new(Memcached::Rails.new), :domain => ENV['google_domain']
+  provider :GoogleApps, OpenID::Store::Memcache.new(Memcached::Rails.new), :domain => CONFIG['google_domain']
 end
