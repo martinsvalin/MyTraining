b774166389cfcd7a555396f8a38f0ce8129685bf
Martin Svalin <martin@lite.nu>
2011-02-10 15:02:30 +0100

    Require uid to be unique for a provider on Authorization

--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -13,5 +13,15 @@ describe Authorization do
     it "should have a provider" do
       Factory.build(:authorization, :provider => nil).should be_invalid
     end
+
+    it "should have unique uid for a provider" do
+      Factory.create(:authorization, :uid => "qwerty", :provider => 'twitter')
+      Factory.build(:authorization, :uid => "qwerty", :provider => 'twitter').should be_invalid
+    end
+
+    it "should not require uid to be unique for different providers" do
+      Factory.create(:authorization, :uid => "qwerty", :provider => 'twitter')
+      Factory.build(:authorization, :uid => "qwerty", :provider => 'facebook').should be_valid
+    end
   end
 end
