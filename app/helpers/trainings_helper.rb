f1c5925a65c84170498837f8371016d82359be93
Martin Svalin <martin@lite.nu>
2011-02-10 15:58:12 +0100

    Removed unnecessary code

--- app/helpers/trainings_helper.rb
+++ app/helpers/trainings_helper.rb
@@ -7,10 +7,6 @@ module TrainingsHelper
     (Date.from_week_within_six_months(@week) + 7.days).cweek
   end
   
-  def this_week
-    Date.from_week_within_six_months(@week).cweek
-  end
-  
   def training_article_class(training)
     if training.person == current_user || training.workout_for_person(current_user)
       "your"
