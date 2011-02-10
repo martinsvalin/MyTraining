e6531ef5a3a98b1eb5d28de8f8b063886d66b2af
Martin Svalin <martin@lite.nu>
2011-02-10 16:36:42 +0100

    Specs for TrainingsHelper

--- spec/helpers/trainings_helper_spec.rb
+++ spec/helpers/trainings_helper_spec.rb
@@ -1,15 +1,47 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TrainingsHelper. For example:
-#
-# describe TrainingsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe TrainingsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "week helpers" do
+    before do
+      @week = 6
+    end
+
+    it "gives me the previous week" do
+      helper.previous_week.should == 5
+    end
+
+    it "gives me the next week" do
+      helper.next_week.should == 7
+    end
+
+    it "gives correct previous week at start of year" do
+      @week = 1
+      helper.previous_week.should == 52
+    end
+
+    it "gives correct next week at end of year" do
+      @week = 52
+      helper.next_week.should == 1
+    end
+  end
+
+  describe "training_article_class" do
+    before do
+      @training = Factory.create(:training)
+      ActionView::Base.any_instance.stubs(:current_user).returns(Factory.build(:person))
+    end
+    it "sets the element class to 'your' for trainings that belong to you" do
+      ActionView::Base.any_instance.stubs(:current_user).returns(@training.person)
+      helper.training_article_class(@training).should == 'your'
+    end
+
+    it "sets the element class to 'other' for trainings that belong to another person" do
+      helper.training_article_class(@training).should == 'other'
+    end
+
+    it "sets the element class to 'everyone' for trainings what belong to no person" do
+      training = Factory.create(:training, :person => nil)
+      helper.training_article_class(training).should == 'everyone'
+    end
+  end
 end
