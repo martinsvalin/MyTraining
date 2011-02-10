05ed58d0afe6b39012e12fc531fe9fe8aa44e304
Martin Svalin <martin@lite.nu>
2011-02-10 13:19:05 +0100

    Cleaned up spec

--- spec/models/date_spec.rb
+++ spec/models/date_spec.rb
@@ -3,15 +3,14 @@ require 'spec_helper'
 describe Date do
   before(:each) do
     Date.stub(:today).and_return(Date.parse("Feb 10, 2011"))
+    @today = Date.today
+    @week_today = 6
+    @year_today = 2011
   end
 
   describe "self.from_week_within_six_months" do
     it "should return a date from a week number" do
-      Date.from_week_within_six_months(20).should be_an_instance_of Date
-    end
-
-    it "should return the first date in the week specified" do
-      Date.from_week_within_six_months(Date.today.cweek).should == Date.today.beginning_of_week
+      Date.from_week_within_six_months(@week_today).should be_an_instance_of Date
     end
 
     it "should return a date in the previous year if the week is more than six months in the future" do
@@ -20,6 +19,12 @@ describe Date do
 
     it "should return a date in the correct week when getting a date in the previous year" do
       Date.from_week_within_six_months(52).cweek.should == 52
+    end
+  end
+  
+  describe "self.first_day_of_week" do
+    it "should return the first date in the week specified" do
+      Date.first_day_of_week(@week_today, @year_today).should == @today.beginning_of_week
     end
   end
 end
