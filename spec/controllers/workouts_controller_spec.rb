b0c2694a2874d74f9b91db12daa6e5a996122b10
Martin Svalin <martin@lite.nu>
2011-02-12 15:52:34 +0100

    Added specs for WorkoutsController

--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -1,56 +1,69 @@
 require 'spec_helper'
 
 describe WorkoutsController do
+  before(:all) do
+    @training = Factory.create(:training)
+    @workout = @training.workouts.new
+  end
+
   describe "POST create" do
+    it "assigns a newly created workout as @workout" do
+      Workout.expects(:new).with(params).returns @workout
+      post :create, :workout => params
+      assigns(:workout).should be @workout
+    end
 
     describe "with valid params" do
-      it "assigns a newly created workout as @workout" do
-        pending
-        Workout.stub(:new).with({'these' => 'params'}) { mock_workout(:save => true) }
-        post :create, :workout => {'these' => 'params'}
-        assigns(:workout).should be(mock_workout)
+      it "saves the workout to the database" do
+        lambda {
+          post :create, :workout => params
+        }.should change{Workout.count}
       end
 
-      it "redirects to the created workout" do
-        pending
-        Workout.stub(:new) { mock_workout(:save => true) }
-        post :create, :workout => {}
-        response.should redirect_to(workout_url(mock_workout))
+      it "redirects to trainings list with success notice" do
+        Workout.expects(:new).returns @workout
+        @workout.expects(:save).returns true
+        post :create, :workout => params
+        response.should redirect_to(trainings_path)
+        flash[:notice].should be_present
       end
     end
 
     describe "with invalid params" do
-      it "assigns a newly created but unsaved workout as @workout" do
-        pending
-        Workout.stub(:new).with({'these' => 'params'}) { mock_workout(:save => false) }
-        post :create, :workout => {'these' => 'params'}
-        assigns(:workout).should be(mock_workout)
+      it "doesn't save the workout to the database" do
+        lambda {
+          post :create, :workout => params.merge({"person_id" => nil}) # person is required
+        }.should_not change{Workout.count}
       end
 
-      it "re-renders the 'new' template" do
-        pending
-        Workout.stub(:new) { mock_workout(:save => false) }
-        post :create, :workout => {}
-        response.should render_template("new")
+      it "redirects to trainings list with failure warning" do
+        Workout.expects(:new).returns @workout
+        @workout.expects(:save).returns false
+        post :create, :workout => params.merge({"person_id" => nil}) # person is required
+        response.should redirect_to(trainings_path)
+        flash[:alert].should be_present
       end
     end
-
   end
 
   describe "DELETE destroy" do
     it "destroys the requested workout" do
-      pending
-      Workout.should_receive(:find).with("37") { mock_workout }
-      mock_workout.should_receive(:destroy)
+      Workout.expects(:find).with("37").returns @workout
+      @workout.expects(:destroy)
       delete :destroy, :id => "37"
     end
 
-    it "redirects to the workouts list" do
-      pending
-      Workout.stub(:find) { mock_workout }
+    it "redirects to the trainings list" do
+      Workout.expects(:find).returns @workout
       delete :destroy, :id => "1"
-      response.should redirect_to(workouts_url)
+      response.should redirect_to(trainings_path)
     end
   end
 
+  def params
+    {
+      "training_id" => @training.id.to_s,
+      "person_id" => "1"
+    }
+  end
 end
